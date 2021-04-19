f35b399f2a3afc7f865b139e9dd2706170dd2070
Thom Bruce <thom@thombruce.com>
2021-04-19 05:53:22 +0100

    Initial Commit

---
+++ layouts/default.vue
@@ -0,0 +1,44 @@
+<template lang='pug'>
+VApp(dark)
+  VNavigationDrawer(v-model='drawer' clipped fixed app)
+    VList
+      VListItem(v-for='(item, i) in items' :key='i' :to='item.to' router exact)
+        VListItemAction
+          VIcon {{ item.icon }}
+        VListItemContent
+          VListItemTitle(v-text='item.title')
+
+  VAppBar(clipped-left fixed app)
+    VAppBarNavIcon(@click.stop='drawer = !drawer')
+    VToolbarTitle(v-text='title')
+
+  VMain
+    VContainer
+      nuxt
+    VFooter
+      VContainer.text-center
+        span &copy; {{ new Date().getFullYear() }}
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      drawer: false,
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Welcome',
+          to: '/'
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Inspire',
+          to: '/inspire'
+        }
+      ],
+      title: 'Vuetify.js'
+    }
+  }
+}
+</script>
---
+++ layouts/error.vue
@@ -0,0 +1,40 @@
+<template lang='pug'>
+VApp(dark)
+  h1(v-if='error.statusCode === 404')
+    | {{ pageNotFound }}
+  h1(v-else)
+    | {{ otherError }}
+  NuxtLink(to='/')
+    | Home page
+</template>
+
+<script>
+export default {
+  layout: 'empty',
+  props: {
+    error: {
+      type: Object,
+      default: null
+    }
+  },
+  data () {
+    return {
+      pageNotFound: '404 Not Found',
+      otherError: 'An error occurred'
+    }
+  },
+  head () {
+    const title =
+      this.error.statusCode === 404 ? this.pageNotFound : this.otherError
+    return {
+      title
+    }
+  }
+}
+</script>
+
+<style lang='scss' scoped>
+h1 {
+  font-size: 20px;
+}
+</style>
