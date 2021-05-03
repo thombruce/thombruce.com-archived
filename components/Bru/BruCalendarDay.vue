498cebb6b387099a065c22e436337f0dab8efe4b
Thom Bruce <thom@thombruce.com>
2021-05-03 13:11:54 +0100

    Add basic calendar page and components

---
+++ components/Bru/BruCalendarDay.vue
@@ -0,0 +1,27 @@
+<template lang='pug'>
+VCard.text-center.fill-height
+  VCardTitle.justify-center.red
+    span.text-h3 {{ days[dateVal.getDay()] }}
+  VCardText
+    span.d-block.text-h1 {{ dateVal.getDate() }}
+    span.d-block.text-h3 {{ months[dateVal.getMonth()] }}
+</template>
+
+<script>
+export default {
+  props: [
+    'date'
+  ],
+  data () {
+    const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+    const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+    const dateVal = this.date ? new Date(this.date) : new Date()
+
+    return {
+      days,
+      months,
+      dateVal
+    }
+  }
+}
+</script>
---
+++ pages/calendar/index.vue
@@ -0,0 +1,13 @@
+<template lang='pug'>
+VRow
+  VCol(cols='12' sm='6' md='4' lg='3')
+    BruCalendarDay
+  VCol
+    VCalendar(:weekdays='[1, 2, 3, 4, 5, 6, 0]' color='red')
+</template>
+
+<script>
+export default {
+  async asyncData({}) {}
+}
+</script>
