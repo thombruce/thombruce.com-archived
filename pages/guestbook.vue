d86d9c45186d5ea049454c61fd1e7fdb7e598381
Thom Bruce <thom@thombruce.com>
2021-05-13 16:13:40 +0100

    Fix Guestbook typo and styles

--- pages/guestbook.vue
+++ pages/guestbook.vue
@@ -3,16 +3,20 @@ VContainer
   article
     h1 Guestbook
     article(v-for='guest in guestbook')
-      h2 {{ guest.name }}
-      time.info--text(:datetime='guest.createdAt') {{ guest.createdAt }}
-      p {{ guest.message }}
+      VCard.mb-3
+        VCardTitle
+          h2.text-h6 {{ guest.name }}
+        VCardSubtitle
+          time.info--text(:datetime='guest.createdAt') {{ guest.createdAt }}
+        VCardText
+          p {{ guest.message }}
     GuestForm
 </template>
 
 <script>
 export default {
   async asyncData({ $content }) {
-    const guestbook = await $content('_date', 'guestbook')
+    const guestbook = await $content('_data', 'guestbook')
       .sortBy('createdAt', 'desc')
       .fetch()
       .catch(() => {})
