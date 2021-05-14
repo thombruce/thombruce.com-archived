3285d3ee0084f918416f0dd0ff7649853a60b94c
Thom Bruce <thom@thombruce.com>
2021-05-14 23:28:40 +0100

    Use PostCSS @import and nesting

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,9 +25,7 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-    '@/assets/nuxt-content.scss'
-  ],
+  css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
