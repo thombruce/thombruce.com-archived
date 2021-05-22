97bb9dd6937584f68e1eb8676adca1b8d268b2d7
Thom Bruce <thom@thombruce.com>
2021-05-22 01:35:35 +0100

    Add additional Timepiece clocks

--- components/timepiece/Timer.vue
+++ components/timepiece/Timer.vue
@@ -1,27 +1,21 @@
 <template lang='pug'>
-span.timepiece.countdown
-  span(v-if='distance !== null && distance <= 0 && expiredText') {{ expiredText }}
-  template(v-else-if='distance')
-    span.days {{ displayDays }}
-    span.daysSeparator(v-if='daysSeparator' v-html='daysSeparator')
-    span.hours {{ displayHours }}
-    span.hoursSeparator(v-if='hoursSeparator' v-html='hoursSeparator')
-    span.minutes {{ displayMinutes }}
-    span.minutesSeparator(v-if='minutesSeparator' v-html='minutesSeparator')
-    span.seconds {{ displaySeconds }}
-    span.secondsSeparator(v-if='secondsSeparator' v-html='secondsSeparator')
+span.timepiece.timer
+  span.days {{ displayDays }}
+  span.daysSeparator(v-if='daysSeparator' v-html='daysSeparator')
+  span.hours {{ displayHours }}
+  span.hoursSeparator(v-if='hoursSeparator' v-html='hoursSeparator')
+  span.minutes {{ displayMinutes }}
+  span.minutesSeparator(v-if='minutesSeparator' v-html='minutesSeparator')
+  span.seconds {{ displaySeconds }}
+  span.secondsSeparator(v-if='secondsSeparator' v-html='secondsSeparator')
 </template>
 
 <script>
 export default {
   props: {
-    // date='2050-03-14'
+    // date='1989-03-14'
     date: {
-      required: true
-    },
-    // expiredText='Expired'
-    expiredText: {
-      type: String
+      default: () => new Date()
     },
     // :leadingZeroes='true'
     // :leadingZeroes='{ hours: true, minutes: true, seconds: true }'
@@ -60,13 +54,13 @@ export default {
   created () {
     var self = this
     // Set the date we're counting down to
-    var countDownDate = new Date(self.date).getTime()
+    var timerDate = new Date(self.date).getTime()
     var x = setInterval(function() {
       // Get today's date and time
       var now = new Date().getTime()
 
       // Find the distance between now and the count down date
-      self.distance = countDownDate - now
+      self.distance = now - timerDate
 
       // Time calculations for days, hours, minutes and seconds
       var days = Math.floor(self.distance / (1000 * 60 * 60 * 24))
@@ -79,11 +73,6 @@ export default {
       self.displayHours = (((self.leadingZeroes === true || self.leadingZeroes['hours']) && hours < 10) ? '0' : '') + hours
       self.displayMinutes = (((self.leadingZeroes === true || self.leadingZeroes['minutes']) && minutes < 10) ? '0' : '') + minutes
       self.displaySeconds = (((self.leadingZeroes === true || self.leadingZeroes['seconds']) && seconds < 10) ? '0' : '') + seconds
-
-      // Text to show if the countdown is finished
-      if (self.distance < 0) {
-        clearInterval(x)
-      }
     }, 1000)
   }
 }
