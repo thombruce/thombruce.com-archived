816b281e9d2b7370ed02bbf11ce1113c7b30efb4
Thom Bruce <thom@thombruce.com>
2021-05-26 00:52:48 +0100

    Add hidden pages to invisible footer

--- pages/index.vue
+++ pages/index.vue
@@ -8,16 +8,21 @@ article
         time(:datetime='article.createdAt') {{ article.createdAt }}
       div
         p {{ article.description }}
+  footer.hidden
+    NuxtLink(v-for='page in pages' :key='page.slug' :to='page') {{ page.title }}
 </template>
 
 <script>
 export default {
   async asyncData({ $content }) {
+    const pages = await $content()
+      .fetch()
+
     const articles = await $content('blog')
       .sortBy('createdAt', 'desc')
       .fetch()
 
-    return { articles }
+    return { pages, articles }
   }
 }
 </script>
