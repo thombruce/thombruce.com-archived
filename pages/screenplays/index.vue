b85a97ee580a148e61e45f44ba8d8acd7fbb6fed
Thom Bruce <thom@thombruce.com>
2021-06-01 02:59:11 +0100

    Remove Screenplays dependency on store

--- pages/screenplays/index.vue
+++ pages/screenplays/index.vue
@@ -16,8 +16,22 @@ article
 
 <script>
 export default {
-  async asyncData({ store }) {
-    const screenplays = await store.dispatch('screenplays/all')
+  async asyncData({ $content }) {
+    const _collection = await $content('screenplays', { deep: true })
+      .sortBy('date', 'asc')
+      .fetch()
+
+    const screenplays = _.orderBy(_collection.reduce((obj, screenplay) => {
+      if (screenplay.dir === '/screenplays') {
+        obj[screenplay.slug] = screenplay
+      } else {
+        let slug = screenplay.dir.split('/').pop()
+        obj[slug] = obj[slug] || { slug, collection: [], date: null }
+        obj[slug].collection.push(screenplay)
+        if (screenplay.date) obj[slug].date = screenplay.date
+      }
+      return obj
+    }, {}), 'date', 'desc')
 
     return { screenplays }
   }
