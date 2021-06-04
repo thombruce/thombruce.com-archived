fe26344caf9b5a70d66e013fac574e4ae4adb45c
Thom Bruce <thom@thombruce.com>
2021-06-04 18:36:44 +0100

    Remove unused plugin file

--- plugins/timepiece.js
+++
@@ -1,7 +0,0 @@
-// TODO: Import of Timepiece is broken. Examine.
-//       At present, components can still be imported individually.
-
-import Vue from 'vue'
-import Timepiece from 'timepiece.vue/src/main.js'
-
-Vue.use(Timepiece)
