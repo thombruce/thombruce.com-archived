713af37939782becfde500e058c6c4006e35c25e
Thom Bruce <thom@thombruce.com>
2021-06-05 16:08:02 +0100

    Upgrade @thombruce/tnt

--- nuxt.config.js
+++ nuxt.config.js
@@ -24,7 +24,6 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    '~plugins/filters.js',
     '~/plugins/staticman'
   ],
 
--- plugins/filters.js
+++
@@ -1,3 +0,0 @@
-import Vue from 'vue'
-
-Vue.filter('titleize', val => _.startCase(val))
