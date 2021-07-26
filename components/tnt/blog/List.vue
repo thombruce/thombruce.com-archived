7c7f001ef4381a7069d532727c469301b025f427
Thom Bruce <thom@thombruce.com>
2021-07-26 22:33:30 +0100

    List articles from all sites

---
+++ components/tnt/blog/List.vue
@@ -0,0 +1,21 @@
+<template lang='pug'>
+div
+  template(v-if='articles')
+    article(v-for='article in articles')
+      header
+        h2
+          a(:href='article.url' target='_blank') {{ article.title }}
+        time(:datetime='article.date_modified') {{ article.date_modified | toLocaleString }}
+      div(v-if='article.summary')
+        p {{ article.summary }}
+  div.text-center(v-else)
+    span.text-2xl.text-secondary There's nothing to see yet.
+</template>
+
+<script>
+export default {
+  props: [
+    'articles'
+  ]
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -7,16 +7,21 @@ article
 
 <script>
 export default {
-  async asyncData ({ $content }) {
+  async asyncData ({ $content, $axios }) {
     const pages = await $content()
       .fetch()
       .catch(() => {})
 
-    let articles = await $content('blog')
-      .where({ draft: { $ne: true } })
-      .sortBy('createdAt', 'desc')
-      .fetch()
-      .catch(() => {})
+    const popcorn = await $axios.$get('https://popcorn.thombruce.com/feed.json')
+    const ink = await $axios.$get('https://ink.thombruce.com/feed.json')
+    const happy = await $axios.$get('https://happy.thombruce.com/feed.json')
+    const undefined = await $axios.$get('https://code.thombruce.com/feed.json')
+    const articles = [
+      ...popcorn.items,
+      ...ink.items,
+      ...happy.items,
+      ...undefined.items
+    ].sort((a,b) => new Date(b.date_modified) - new Date(a.date_modified))
     
     return { pages, articles }
   },
