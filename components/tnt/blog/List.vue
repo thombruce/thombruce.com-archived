6ee65193651878a7c464130e975d295b0a185848
Thom Bruce <thom@thombruce.com>
2021-08-04 13:53:01 +0100

    Show images from feed

--- components/tnt/blog/List.vue
+++ components/tnt/blog/List.vue
@@ -1,16 +1,20 @@
 <template lang='pug'>
 div
   template(v-if='articles')
-    article(v-for='article in articles')
-      header
-        h2
-          a(:href='article.url' target='_blank') {{ article.title }}
-          |
-          |
-          span.badge {{ site(article.url) | titleize }}
-        time(:datetime='article.date_modified') {{ article.date_modified | toLocaleString }}
-      div(v-if='article.summary')
-        p {{ article.summary }}
+    article.card.card-side.bordered.mb-4(v-for='article in articles')
+      figure
+        NuxtImg(v-if='article.image' :src='article.image' width='200' height='200' style='width:200px;height:200px;object-fit:cover;')
+
+      .card-body
+        header
+          h2.card-title
+            a(:href='article.url' target='_blank') {{ article.title }}
+            |
+            |
+            span.badge {{ site(article.url) | titleize }}
+          time(:datetime='article.date') {{ article.date | toLocaleString }}
+        div(v-if='article.summary')
+          p {{ article.summary }}
   div.text-center(v-else)
     span.text-2xl.text-secondary There's nothing to see yet.
 </template>
