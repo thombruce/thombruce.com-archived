eb7c7611f44a5a74552d25801d8691fda58d368e
Thom Bruce <thom@thombruce.com>
2021-08-23 22:26:50 +0100

    Update Nuxt TNT

--- nuxt.config.js
+++ nuxt.config.js
@@ -56,7 +56,7 @@ export default {
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
-    "@thombruce/tnt",
+    "@thombruce/nuxt-tnt",
     "@thombruce/vue-timepiece/nuxt",
     "@thombruce/vue-fountain/nuxt",
     "@thombruce/vue-playfair/nuxt"
